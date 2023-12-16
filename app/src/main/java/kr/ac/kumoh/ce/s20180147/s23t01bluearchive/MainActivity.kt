a3f928f7da56ae5d0e8d308d1b3dba8431bf97ca
ZeppIine <kim0904min@gmail.com>
2023-12-16 19:47:24 +0900

    modify MainActivity

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23t01bluearchive/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23t01bluearchive/MainActivity.kt
@@ -3,44 +3,47 @@ package kr.ac.kumoh.ce.s20180147.s23t01bluearchive
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import kr.ac.kumoh.ce.s20180147.s23t01bluearchive.ui.theme.S23T01BlueArchiveTheme
 
 class MainActivity : ComponentActivity() {
+    private val viewModel: StudentViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            S23T01BlueArchiveTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MainScreen(viewModel)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(viewModel: StudentViewModel) {
+    val studentList by viewModel.studentList.observeAsState(emptyList())
+
+    S23T01BlueArchiveTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            StudentList(studentList)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    S23T01BlueArchiveTheme {
-        Greeting("Android")
+fun StudentList(studentList: List<Student>) {
+    Column {
+        Text(studentList.toString())
     }
 }
