d1edd3c9974c5ea09b62b12550ff53cd807870cb
ZeppIine <kim0904min@gmail.com>
2023-12-16 19:32:58 +0900

    add student data class

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23t01bluearchive/student.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20180147.s23t01bluearchive
+
+data class student(
+    val id: Int,
+    val name: String,
+    val academy: String,
+    val club: String,
+    val star: Int,
+    val attack: String,
+    val defence: String
+)
