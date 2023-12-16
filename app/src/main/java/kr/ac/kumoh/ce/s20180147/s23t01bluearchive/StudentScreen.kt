ee3dc1587251908b96145ecb56f59a5c71139ce3
ZeppIine <kim0904min@gmail.com>
2023-12-16 22:20:16 +0900

    modify StudentScreen

--- app/src/main/java/kr/ac/kumoh/ce/s20180147/s23t01bluearchive/StudentScreen.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20180147/s23t01bluearchive/StudentScreen.kt
@@ -14,20 +14,31 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import coil.compose.AsyncImage
 
 enum class StudentScreen {
@@ -46,8 +57,15 @@ fun StudentApp(studentList: List<Student>) {
         composable(route = StudentScreen.List.name) {
             StudentList(navController, studentList)
         }
-        composable(route = StudentScreen.Detail.name) {
-//            StudentDetail(navController, studentList)
+        composable(
+            route = StudentScreen.Detail.name + "/{id}",
+            arguments = listOf(navArgument("id") {
+                type = NavType.IntType
+            })
+        ) {
+            val id = it.arguments?.getInt("id") ?: -1
+            if(id >= 0)
+                StudentDetail(studentList[id])
         }
     }
 }
@@ -68,7 +86,7 @@ fun StudentList(navController: NavController, students: List<Student>) {
 fun StudentItem(navController: NavController, student: List<Student>, id: Int){
     Card(
         modifier = Modifier.clickable {
-            navController.navigate(StudentScreen.Detail.name)
+            navController.navigate(StudentScreen.Detail.name + "/$id")
         },
         elevation = CardDefaults.cardElevation(8.dp)
     ) {
@@ -102,8 +120,82 @@ fun StudentItem(navController: NavController, student: List<Student>, id: Int){
 fun TextAcademy(academy: String){
     Text(academy, fontSize = 20.sp)
 }
+
 @Composable
 fun TextName(name: String){
     Text(name, fontSize = 30.sp)
 }
 
+@Composable
+fun RatingBar(stars: Int) {
+    Row {
+        repeat(stars) {
+            Icon(
+                imageVector = Icons.Filled.Star,
+                contentDescription = "성급",
+                modifier = Modifier.size(48.dp),
+                tint = Color.Yellow)
+        }
+    }
+}
+
+@Composable
+fun StudentDetail(student: Student) {
+    Card {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            RatingBar(student.star)
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text(
+                student.name,
+                fontSize = 40.sp,
+                textAlign = TextAlign.Center,
+                lineHeight = 45.sp
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            AsyncImage(
+                model = "https://picsum.photos/300/300?random=${student.id}",
+                contentDescription = "노래 앨범 이미지",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(400.dp),
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                AsyncImage(
+                    model = "https://i.pravatar.cc/100?u=${student.academy}",
+                    contentDescription = "가수 이미지",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(50.dp)
+                        .clip(CircleShape)
+                )
+                Text(student.academy, fontSize = 30.sp)
+            }
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Text(
+                student.attack,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                lineHeight = 35.sp
+            )
+
+            Text(
+                student.defence,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                lineHeight = 35.sp
+            )
+        }
+    }
+}
